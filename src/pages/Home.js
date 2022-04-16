5aa83359a8bcef9181baaedb3afed87723e92d1a
MattCornicDev <glgharib@gmail.com>
2022-04-17 01:53:47 +0200

    Route

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+           <p>Ici cest la page d'accueil</p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Knowledges.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Knowledges = () => {
+    return (
+        <div>
+            Ici c'est la page connaissance
+        </div>
+    );
+};
+
+export default Knowledges;
