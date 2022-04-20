31d81bc466624d1a4f584b9020d27e5e5cb76ce2
MattCornicDev <glgharib@gmail.com>
2022-04-20 20:30:48 +0200

    bouton infos

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -1,6 +1,20 @@
 import React, {Component} from 'react';
 
 class Project extends Component {
+    state = {
+        showInfo: false
+    }
+    handleInfo = () => {
+        this.setState({
+            showInfo:!this.state.showInfo
+        });
+    }
+    // handleExit = () => {
+    //     this.setState({
+    //         showInfo:!this.state.showInfo
+    //     });
+    // }
+
     render() {
         let {name, languagesIcons, source, info, picture} = this.props.item;
         return (
@@ -10,8 +24,21 @@ class Project extends Component {
                     <i className={icon} key={icon}></i> )}
                 </div>
                 <h3>{name}</h3>
-                <img src={picture} alt="image du site"/>
-                <span className="info"><i className="fas fa-plus-circle"></i></span>
+                <img src={picture} alt="image du site" onClick={this.handleInfo}/>
+                <span className="infos" onClick={this.handleInfo}>
+                    <i className="fas fa-plus-circle"></i></span>
+
+                {
+                    this.state.showInfo && (
+                        <div className="showInfos">
+                            <div className="infosContent">
+                                <div className="head">
+                                    <h2>{ name }</h2>
+                                </div>
+                            </div>Content
+                        </div>
+                    )
+                }
             </div>
 
         );
