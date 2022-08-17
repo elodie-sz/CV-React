782c2524b99e019dbe712254982f610881f73200
MattCornicDev <glgharib@gmail.com>
2022-08-17 18:10:41 +0200

    Phrases d'accroches

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,9 +9,11 @@ const Home = () => {
                 <div className="content">
                     <h2>Développeurs Full Stack</h2>
 
-
-                    <p>Parce que le développement informatique évolue constament, parce que vous avez besoin de maintenir vos API à jour, parce que vous recherchez des développeurs compétents, passionnés qui savent travailler en équipe "WebDev" s'est constitué. Que ce soit principalement en front-end avec React nous maintenons ou créons vos applis ou que ce soit en back-end grace à Symfony vos Applications PHP prendront une autre dimension. Vous avez dit NodeJS ! Aucun soucis nous collaborons pour développer des spécialités et de répondre au mieux aux besoins de nos client. Développons ensemble vos applications qu'elle soit en cours ou pour démarrer nouveau projet React, Symfony ou NodeJS. Nous avons choisi de nous associé pour les projets à venir afin de nous épauler dans vos besoins, et répondre à plus de language de programmation. </p>
-
+                    <p>Parce que le développement informatique évolue constament, parce que vous avez besoin de maintenir vos APIs à jour, parce que vous recherchez des développeurs compétents, passionnés qui savent travailler en équipe <strong>Enodev</strong> s'est constitué.
+                        <br/><br/>
+                        Que ce soit React NodeJS ou Symfony nous maintenons, créons vos applications PHP ou JavaScript.<br/><br/>
+                        Notre collaboration est issue d'une volonté à répondre le plus efficacement possible aux besoins de nos clients.<br/><br/>
+                        Développons ensemble vos APIs qu'elle soit en cours ou démarrage React, Symfony ou NodeJS, nous répondrons au mieux à vos attentes.</p>
 
                     <div className="pdf">
                         <a href="./media/resume.pdf" target="_blank">CV de Rarib LAAMIMAT</a>
