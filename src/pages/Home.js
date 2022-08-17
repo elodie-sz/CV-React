8374bbc72abe088e4a59427d4fdc9808cc5af4a3
MattCornicDev <glgharib@gmail.com>
2022-08-17 17:42:00 +0200

    redaction du text premier jet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
                     <h2>Développeurs Full Stack</h2>
 
 
-                    <p>Paragrapheeeeeeeeeeeeeeeee</p>
+                    <p>Parce que le développement informatique évolue constament, parce que vous avez besoin de maintenir vos API à jour, parce que vous recherchez des développeurs compétents, passionnés qui savent travailler en équipe "WebDev" s'est constitué. Que ce soit principalement en front-end avec React nous maintenons ou créons vos applis ou que ce soit en back-end grace à Symfony vos Applications PHP prendront une autre dimension. Vous avez dit NodeJS ! Aucun soucis nous collaborons pour développer des spécialités et de répondre au mieux aux besoins de nos client. Développons ensemble vos applications qu'elle soit en cours ou pour démarrer nouveau projet React, Symfony ou NodeJS. Nous avons choisi de nous associé pour les projets à venir afin de nous épauler dans vos besoins, et répondre à plus de language de programmation. </p>
 
 
                     <div className="pdf">
