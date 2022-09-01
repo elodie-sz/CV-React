bf5614f5f1f912eebaf42463fc83ecfbbab07590
MattCornicDev <glgharib@gmail.com>
2022-09-01 23:18:57 +0200

    Dropdown

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {NavLink, navLink} from 'react-router-dom';
+import Dropdown from "react-bootstrap/Dropdown";
 
 const Navigation = () => {
     return (
+
         <div className="sidebar">
             <div className="id">
                 <div className="idContent">
@@ -20,13 +22,86 @@ const Navigation = () => {
                             <span>Accueil</span>
                         </NavLink>
                     </li>
+                    <div className="navigation">
 
-                    {/* <li>
-                        <NavLink exact="true" to="/savoirFaire" activeClassName="navActive">
-                            <i className="fas fa-mountain"></i>
-                            <span>Savoir-faire</span>
-                        </NavLink>
-                    </li>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="success" id="dropdown-elodie">
+                                Elodie
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item>
+                                    <li>
+                                    <NavLink exact="true" to="/competencesEK" activeClassName="navActive">
+                                        <i className="fas fa-mountain"></i>
+                                        <span>Compétences Elodie KSIAZEK</span>
+                                    </NavLink>
+                                    </li>
+                                </Dropdown.Item>
+                                <Dropdown.Item >
+                                    <li>
+                                    <NavLink exact="true" to="/portfolioEK" activeClassName="navActive">
+                                        <i className="fas fa-images"></i>
+                                        <span>Portfolio Elodie KSIAZEK</span>
+                                    </NavLink>
+                                    </li>
+                                </Dropdown.Item>
+                                <Dropdown.Item >
+                                    <li>
+                                    <NavLink exact="true" to="/contactEK" activeClassName="navActive">
+                                        <i className="fas fa-address-book"></i>
+                                        <span>Contact <br/> Elodie KSIAZEK</span>
+                                    </NavLink>
+                                    </li>
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+
+                        <Dropdown>
+                            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                Rarib
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item>
+                                    <li>
+                                    <NavLink exact="true" to="/competences" activeClassName="navActive">
+                                        <i className="fas fa-mountain"></i>
+                                        <span>Compétences</span>
+                                    </NavLink>
+                                    </li>
+                                    </Dropdown.Item>
+                                <Dropdown.Item href="#/action-1">
+                                    <li>
+                                    <NavLink exact="true" to="/portfolio" activeClassName="navActive">
+                                        <i className="fas fa-images"></i>
+                                        <span>Portfolio</span>
+                                    </NavLink>
+                                    </li>
+                                </Dropdown.Item>
+                                <Dropdown.Item href="#/action-1">
+                                    <li>
+                                    <NavLink exact="true" to="/contact" activeClassName="navActive">
+                                        <i className="fas fa-address-book"></i>
+                                        <span>Contact</span>
+                                    </NavLink>
+                                    </li>
+                                </Dropdown.Item>
+                                <Dropdown.Item>
+                                    <li>
+                                        <NavLink exact="true" to="/savoirFaire" activeClassName="navActive">
+                                            <i className="fas fa-mountain"></i>
+                                            <span>Savoir-faire</span>
+                                        </NavLink>
+                                    </li>
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+
+                    </div>
+
+
+                    {/*
                     <li>
                         <NavLink exact="true" to="/savoirFaireEK" activeClassName="navActive">
                             <i className="fas fa-mountain"></i>
@@ -34,42 +109,6 @@ const Navigation = () => {
                         </NavLink>
                     </li> */}
 
-                    <li>
-                        <NavLink exact="true" to="/competences" activeClassName="navActive">
-                            <i className="fas fa-mountain"></i>
-                            <span>Compétences</span>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact="true" to="/competencesEK" activeClassName="navActive">
-                            <i className="fas fa-mountain"></i>
-                            <span>Compétences Elodie KSIAZEK</span>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact="true" to="/portfolio" activeClassName="navActive">
-                            <i className="fas fa-images"></i>
-                            <span>Portfolio</span>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact="true" to="/portfolioEK" activeClassName="navActive">
-                            <i className="fas fa-images"></i>
-                            <span>Portfolio Elodie KSIAZEK</span>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact="true" to="/contact" activeClassName="navActive">
-                            <i className="fas fa-address-book"></i>
-                            <span>Contact</span>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact="true" to="/contactEK" activeClassName="navActive">
-                            <i className="fas fa-address-book"></i>
-                            <span>Contact <br/> Elodie KSIAZEK</span>
-                        </NavLink>
-                    </li>
                 </ul>
             </div>
 
