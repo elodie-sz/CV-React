b57d66b8ad40f8d16f1976f0ae3c7c5bf6889445
elodie-sz <elodie.siaze@gmail.com>
2022-09-01 10:00:08 +0200

    ajout de savoir etre à la place des interets

--- src/components/knowledgesEK/HobbiesEK.js
+++ src/components/knowledgesEK/HobbiesEK.js
@@ -6,15 +6,27 @@ const HobbiesEK = () => {
 
             <div className="hobbies">
 
-                <h3>Interêts</h3>
+                <h3>Savoir-être</h3>
 
                 <br/>
 
-                <ul>
+                 <ul>
+                    <li><i className="fas fa-check-square"></i><span>Autonomie</span></li>
+                    <li><i className="fas fa-check-square"></i><span>Capacité d'adaptation</span></li>
+                    <li><i className="fas fa-check-square"></i><span>Sens de l'organisation</span></li>
+                    <li><i className="fas fa-check-square"></i><span>Force de proposition</span></li>
+                    <li><i className="fas fa-check-square"></i><span>Persévérance</span></li>
+                </ul>
+
+
+
+                {/* <ul>
                     <li className="hobby"><i className="fas fa-futbol"></i><span>Football</span></li>
                     <li className="hobby"><i className="fas fa-dumbbell"></i><span>Fitness</span></li>
                     <li className="hobby"><i className="fas fa-plane"></i><span>Voyages</span></li>
-                </ul>
+                </ul> */}
+
+
             </div>
         </div>
     );
